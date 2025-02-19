c706efca196156176d1326784db149f0ee528a0a
t-ros <diogo.mm.monteiro@gmail.com>
2025-02-19 20:56:59 +0000

    feat: add program to repeat number n times

---
+++ Intermediate/repeat_n_times.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*  
+ * Program: 
+ * Description: This programs keeps prompting for an odd number > 1
+ * 				Returns the same number for the same amount of times
+ * Example: Input: 5
+ * 			Output: 5 5 5 5 5 
+ * Author: Teros  
+ */
+ 
+int main(){
+	int num;
+	
+	do{
+	printf("Give me an odd number grater than 1: ");
+	scanf("%d", &num);
+	} while (num <= 1 || num%2 == 0);
+	
+	for(int i=0; i<num; i++){
+		printf("%d ", num);
+	}
+	
+	return 0;
+}
