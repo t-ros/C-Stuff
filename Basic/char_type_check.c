2ae1605ccc78e80810bcb60c378a2b563aebbcde
t-ros <diogo.mm.monteiro@gmail.com>
2025-02-19 20:15:38 +0000

    feat: check char type
    This program prompts the user to enter a single character.
    It checks what type of character it is
    Only recognizes letters and digits

---
+++ Basic/char_type_check.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+ * Program: Check Char Type
+ * Description: This program prompts the user to enter a single character.  
+ *              It checks what type of character it is 
+ *              Only recognizes letters and digits  
+ * Author: Teros
+ */
+int main(){
+	
+	char a;
+	
+	printf("Enter a letter: ");
+	scanf(" %c", &a);
+	
+	if( a >= 'a' && a <= 'z'){
+		printf("'%c' is a lowercase letter", a);			
+	} else if (a >= 'A' && a <= 'Z') {
+		printf("'%c' is an Uppercase letter", a);		
+	} else if (a >= '0' && a <= '9'){
+		printf("%c is a digit", a);
+	} else printf("That's not a digit!");
+	
+	return 0;
+}
+
+
