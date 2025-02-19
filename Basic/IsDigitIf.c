959c5d47a96c157b85683c8f0875ff4a1488369a
t-ros <diogo.mm.monteiro@gmail.com>
2025-02-19 19:17:20 +0000

    Create IsDigitIf.c

---
+++ Basic/IsDigitIf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * Program: Is Digit w/ If Else
+ * Description: Check if a given character is a digit (0-9)
+ * 				Has to be done with an If Else
+ * Author: Teros
+ */
+
+int main(){
+	char a;
+	
+		printf("Enter a char: ");
+		scanf(" %c", &a);
+	
+	if (a >= '0' && a <= '9'){
+		printf("-> %c is a digit", a);
+	} else printf("That's not a digit!");
+	
+	return 0;
+}
