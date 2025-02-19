7993d975db2ac7789d7b2e9c8f313072d2ffb58c
t-ros <diogo.mm.monteiro@gmail.com>
2025-02-19 19:40:03 +0000

    Create is_upper_case.c

---
+++ Basic/is_upper_case.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * Program: Is Upper
+ * Description: Checks if a given character is an Uppercase or Else
+ * 				
+ * Author: Teros
+ */
+
+int main(){
+	char a;
+	
+	printf("Enter a char: ");
+	scanf(" %c", &a);
+	
+	if (a >= 'A' && a <= 'Z'){
+		printf("-> %c is an uppercase letter", a);
+	} else printf("That's not an uppercase letter");
+	
+	return 0;
+}
